6e7d47d1a061a264420e4532b5cc072422986437
CyborgDitz <sebastian.ditz@student.forsbergsskola.se>
2025-02-17 20:15:25 +0100

    Struct data changed from position vector to intx, y
    etc

--- playerData.h
+++ playerData.h
@@ -1,9 +1,10 @@
 #include "common.h"
 #ifndef PLAYERDATA_H
 #define PLAYERDATA_H
-struct player {
-    Vector2 position;
-    Vector2 size;
-    int health;
-};
+typedef struct Player{
+    int x,y;
+    int health {100};
+    Player(int startX = 0, int startY = 0, int startHealth = 100)
+       : x(startX), y(startY), health(startHealth) {}
+} player;
 #endif //PLAYERDATA_H
